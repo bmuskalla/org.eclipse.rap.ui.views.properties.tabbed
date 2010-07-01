d016c391638e8f6bea07aebb5fab1e3274b41679
Benjamin Muskalla <bmuskalla@eclipsesource.com>
2010-07-01 13:12:07 +0200

    fixed projects setup

--- org.eclipse.ui.examples.views.properties.tabbed/src/org/eclipse/ui/examples/views/properties/tabbed/article/views/SizeSection.java
+++ org.eclipse.ui.examples.views.properties.tabbed/src/org/eclipse/ui/examples/views/properties/tabbed/article/views/SizeSection.java
@@ -26,6 +26,7 @@ import org.eclipse.ui.views.properties.tabbed.AbstractPropertySection;
 import org.eclipse.ui.views.properties.tabbed.ITabbedPropertyConstants;
 import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
 
+
 /**
  * The size section on the button tab.
  * 
